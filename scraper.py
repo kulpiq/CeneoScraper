f9a09abb920d8b9a5ca1b95aba5988d7630471a8
kulpiq <dominika.kulpa777@gmail.com>
2023-04-25 18:03:18 +0200

    2023.04.25 DK

--- scraper.py
+++ scraper.py
@@ -1,72 +1,104 @@
 import json
+import os
 import requests
+import numpy as np
 from bs4 import BeautifulSoup
+from translate import Translator
 
-def get_something(dom_tree, selector, attribute = None):
+def get_element(dom_tree, selector=None, attribute=None, return_list = None):
     try:
+        if return_list:
+            return ", ".join([tag.text.strip() for tag in dom_tree.select("div.review-feature__col:has( > div.review-feature__title--positives) > div.review-feature__item")])
         if attribute:
-            return dom_tree.select_one(selector)[attribute].strip()
+            if selector:
+                return dom_tree.select_one(selector)[attribute].strip()
+            return dom_tree[attribute]
         return dom_tree.select_one(selector).text.strip()
-    except ArithmeticError:
+    
+    except (AttributeError, TypeError):
         return None
-#product_code = input("Please enter product code: ")
-product_code = "129901214"
-print(product_code)
-
+def clean_text(text):
+     
+    return " ".join(text.replace(r"\s", " ").split())
+selectors = {
+                    "opinion_id": [None, "data-entry-id"],
+                    "author": ["span.user-post__author-name"],
+                    "recommendation": ["span.user-post__author-recommendation > em"],
+                    "score": ["span.user-post__score-count"],
+                    "description": ["div.user-post__text"],
+                    "pros": ["div.review-feature__col:has( > div.review-feature__title--positives) > div.review-feature__item",None, True],
+                    "cons": ["div.review-feature__col:has( > div.review-feature__title--negatives) > div.review-feature__item"],
+                    "like": ["button.vote-yes > span"],
+                    "dislike": ["button.vote-yes > span"],
+                    "publish_date": ["span.user-post__published > time:nth-child(1)", "datetime"],
+                    "purchase_date": ["span.user-post__published > time:nth-child(2)", "datetime"]
+                }
+from_lang = 'pl'
+to_lang = "eng"
+translator = Translator(from_lang, to_lang)
+#product_code = input("Please enter the product code: ")
+product_code = "129910940"
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+all_opinions = []
 
 while url:
-
     response = requests.get(url)
 
     if response.status_code == requests.codes.ok:
-        page_dom = BeautifulSoup(response.text, "html.parser")
+        print(url)
+        page_dom = BeautifulSoup(response.text, 'html.parser')
+        print(get_element(page_dom))
         opinions = page_dom.select("div.js_product-review")
-        print(len(opinions))
-        if len(opinions) >0:
-            print(f"There are options about product with {product_code} code.")
-            all_opinions =[]
+        
+        if len(opinions) > 0:
+
             for opinion in opinions:
 
-                opinion_id = opinion["data-entry-id"]
-                author = opinion.select_one("span.user-post__author-name").text.strip()
-                try: 
-                    recomendation = opinion.select_one("span.user-post__author-recomendation > em").text.strip()
-                except AttributeError:
-                    recomendation = None
-                score = opinion.select_one("span.user-post__score-count").text.strip()
-                description = opinion.select_one("div.user-post__text").text.strip()
-                pros = opinion.select("div.review-feature__col:has( > div.review-feature__title--positives)> div.review-feature__item")
-                pros = [p.text.strip() for p in pros]
-                cons = opinion.select("div.review-feature__col:has( > div.review-feature__tite--negatives)> div.review-feature__item")
-                cons = [c.text.strip() for c in cons]
-                like =  opinion.select_one("button.vote-yes > span").text.strip()
-                dislike =  opinion.select_one("button.vote-no > span").text.strip()
-                publish_date = opinion.select_one("span.user-post__published >time:nth-child(1)")["datetime"].strip()
-                try:
-                    purchase_date = opinion.select_one("span.user-post__published >time:nth-child(2)")["datetime"].strip()
-                except AttributeError:
-                    purchase_date = None
-                
-                single_opinion = {
-                    "opinion_id": opinion_id,
-                    "author": author,
-                    "recomendation": recomendation,
-                    "score": score,
-                    "description": description,
-                    "pros": pros,
-                    "cons": cons,
-                    "like": like,
-                    "dislike": dislike,
-                    "publish_date": publish_date,
-                    "purchase_date": purchase_date
-                } 
+                # opinion_id = opinion["data-entry-id"]
+                # author = get_element(opinion, "span.user-post__author-name")
+                # recommendation = get_element(opinion, "span.user-post__author-recommendation > em")
+                # score = get_element(opinion, "span.user-post__score-count")
+                # description = get_element(opinion, "div.user-post__text")
+                # pros = get_element(opinion, "div.review-feature__col:has( > div.review-feature__title--positives) > div.review-feature__item",None, True)
+                # pros = [p.text.strip() for p in pros]
+                # cons = opinion.select("div.review-feature__col:has( > div.review-feature__title--negatives) > div.review-feature__item")
+                # cons = [c.text.strip() for c in cons]
+                # like = get_element(opinion, "button.vote-yes > span")
+                # dislike = get_element(opinion, "button.vote-yes > span")
+                # publish_date = get_element(opinion, "span.user-post__published > time:nth-child(1)", "datetime")
+                # purchase_date = get_element(opinion, "span.user-post__published > time:nth-child(2)", "datetime")
+
+                single_opinion = {}
+                for key,value in selectors.items():
+                    single_opinion[key] = get_element(opinion, *value)
+
+                single_opinion["recommendation"] == True if single_opinion["recommendation"] == "Polecam" else False if single_opinion["recommendation"] == "Nie polecam" else None
+                single_opinion["score"] = np.divide(*[float(score.replace(",",".")) for score in single_opinion["score"].split("/")])
+                single_opinion["like"] = int(single_opinion["like"])
+                single_opinion["dislike"] = int(single_opinion["dislike"])
+                single_opinion["description"] = clean_text(single_opinion["description"])
+                single_opinion["description-en"] = translator.translate(single_opinion["description"][:500])
+                single_opinion["pros-en"] = translator.translate(single_opinion["pros"])
+                single_opinion["cons-en"] = translator.translate(single_opinion["cons"])
+
+
                 all_opinions.append(single_opinion)
-            try:    
-                url = "https://www.ceneo.pl"+ page_dom.select_one("a.pagination__next")["href"]
+            
+            try:
+                page = get_element(page_dom,"a.pagination__next", "href")
+                url = f"https://www.ceneo.pl/{page}"
             except TypeError:
                 url = None
             print(url)
-        if len(all_opinions) >  0:
-            with open(f"./opinions/{product_code}.json","w",encoding="UTF-8") as jf:
-                json.dump(all_opinions,jf, indent=4,ensure_ascii=False)
+        else:
+            print(f"There are no opinions about produsct {product_code} code")
+    else:
+        print("The product does not exist")
+    
+    if len(all_opinions) > 0:
+        if not os.path.exists("./opinions"):
+            os.mkdir("./opinions")
+        with open(f"./opinions/{product_code}.json", "w", encoding = "UTF-8") as jf:
+            json.dumb(all_opinions, jf, indent = 4, ensure_ascii = False)
+    
+            
